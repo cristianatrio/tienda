bdcc98ed72438e66285fc603dca0095cdff945c6
Cristian Atrio <cristianatrio91@gmail.com>
2022-05-29 10:28:46 -0300

    cambios en firebase 2

--- src/views/ProductTipo.jsx
+++ src/views/ProductTipo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useLocation, useParams } from 'react-router-dom'
-import ItemListContainer from './../components/ItemListContainer/ItemListContainer.jsx';
+import ItemListContainer from './../components/itemListContainer/ItemListContainer.jsx';
 
 const ProductTipo = () => {
   const location = useLocation()
